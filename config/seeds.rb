71204db43079b544b671354bcabdef44ee1e1f09
Graham Marlow <mgmarlow@hey.com>
2022-01-05 17:55:14 -0800

    Improve event generation

--- config/seeds.rb
+++ config/seeds.rb
@@ -7,31 +7,38 @@ puts "Creating locations..."
 locations = world["locations"].map { |l| Location.create(name: l) }
 
 def create_date
-  Faker::Date.between(from: '2010-04-05', to: '2022-04-12')
+  Faker::Date.between(from: '2021-04-05', to: '2021-04-12')
     .strftime("%Y%m%d")
 end
 
-def create_time
-  start_time = (0..2300).step(100).to_a.sample
-  end_time = (start_time..2400).step(100).to_a.sample
-
-  [start_time, end_time]
+# Each event occurred 5 times during the week at random 1-hr intervals.
+def create_time_windows
+  (0..2300).step(100).to_a.sample(5).map do |start_time|
+    [create_date, start_time, start_time + 100]
+  end
 end
 
 puts "Creating events..."
 world["events"].each do |data|
   location = Location.find_by(name: data["location"])
 
-  people.sample(100).each do |person|
-    start_time, end_time = create_time
+  events = create_time_windows.map do |window|
+    date, start_time, end_time = window
 
-    person.events << Event.create(
+    Event.create(
       name: data["name"],
       location: location,
-      date: create_date,
+      date: date,
       start_time: start_time,
       end_time: end_time,
     )
+  end
+
+  # Add 15 people to each event
+  events.each do |event|
+    people.sample(15).each do |person|
+      person.events << event
+    end
   end
 end
 
@@ -41,6 +48,9 @@ important_people = people_with_events.sample(3)
 perp = important_people.last
 puts "Created perp: #{perp.name}"
 
-witnesses = important_people.take(2)
+# Find any people that went to the same event.
+witnesses = perp.events.flat_map do |event|
+  Event.find(event.id).people
+end
 puts "#{witnesses.length} witnesses: #{witnesses.pluck(:name).join(" and ")}"
 
